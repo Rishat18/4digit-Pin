f137818bde7487dcc173f25053a382036d0c9372
Rishat18 <abdullahrishat008@gmail.com>
2022-02-14 23:45:21 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function getPin(){
+    const pin = Math.round(Math.random()*10000);
+    const pinString = pin + '';
+    if (pinString.length == 4){
+        return pin ;
+    }
+    else {
+        console.log('3 digit haber call karba')
+        return getPin();
+    }
+}
+
+function generatePin(){
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+    console.log(pin)
+}
