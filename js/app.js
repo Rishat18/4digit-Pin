defb14fc05d76cecb9d164b7f48298a63f7c61bb
Rishat18 <abdullahrishat008@gmail.com>
2022-02-15 19:10:19 +0600

    Pin matching system added

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ function getPin(){
         return pin;
     }
    else{
-       console.log('3 digit')
+    //    console.log('3 digit')
        return getPin();
    }
 }
@@ -14,7 +14,7 @@ function generatePin(){
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
-// type the pin code
+// Type the pin code in the input
 document.getElementById('key-pad').addEventListener('click', function(event){
     const number = event.target.innerText;
     const calcInput = document.getElementById('typed-numbers');
@@ -30,7 +30,22 @@ document.getElementById('key-pad').addEventListener('click', function(event){
     }
 })
 
-
+// Matching pin period
+function verifyPin(){
+    const generatePin = document.getElementById('display-pin').value;
+    const pinNumber = document.getElementById('typed-numbers').value;
+    const successMessage = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-fail');
+    if (generatePin === pinNumber){
+        successMessage.style.display = 'block';
+        failError.style.display = 'none';
+        
+    }
+    else{
+        failError.style.display = 'block';
+        successMessage.style.display = 'none';
+    }
+}
 
 
 
